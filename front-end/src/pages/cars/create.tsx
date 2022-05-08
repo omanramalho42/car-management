3f91a6f31ca4de0ab867d9fd33c90de22bdd154d
Oman Ramalho <omanapple42@hotmail.com>
2022-05-08 18:21:33 -0300

    alterando rotas de fetch de dados

--- front-end/src/pages/cars/create.tsx
+++ front-end/src/pages/cars/create.tsx
@@ -56,7 +56,7 @@ const schema = yup.object({
         .integer("O ano de fabricação tem que ser um caractere alfanúmerico inteiro")
         .required("A data de fabricação é obrigatória")
         .min(4, "O ano de fabricação tem que possuir 4 digitos"),
-        // .max(4, "O ano de fabricação tem que possuir 4 digitos"),
+    // .max(4, "O ano de fabricação tem que possuir 4 digitos"),
     type: yup.string().required(),
     version: yup.string().required(),
     form: yup.string().required(),
@@ -67,215 +67,215 @@ const schema = yup.object({
 
 const Create: NextPage = () => {
 
-  const router = useRouter()
+    const router = useRouter()
 
-  const { register, handleSubmit, formState: { errors } } = useForm<CreateCarForm>({
-    resolver: yupResolver(schema),
-    defaultValues: {
-        brand: '',
-        model: '',
-        sign: '',
-        fabrication: 1900,
-        form: '',
-        type: '',
-        version: '',
-        // color: [false,false,false,false,false]
-    }
-  })
+    const { register, handleSubmit, formState: { errors } } = useForm<CreateCarForm>({
+        resolver: yupResolver(schema),
+        defaultValues: {
+            brand: '',
+            model: '',
+            sign: '',
+            fabrication: 1900,
+            form: '',
+            type: '',
+            version: '',
+            // color: [false,false,false,false,false]
+        }
+    })
 
-  const [success, setSuccess] = useState('null')
+    const [success, setSuccess] = useState('null')
 
-  const handleSubmitForm: SubmitHandler<CreateCarForm> = async (values) => {
-    console.log(values)
-    const { brand, model, sign, fabrication, form, type, version } = values
-    try {
-        const response = await fetch('http://localhost:3001/create-car', {
-            method: 'post',
-            headers: {
-                Accept: 'application/json',
+    const handleSubmitForm: SubmitHandler<CreateCarForm> = async (values) => {
+        console.log(values)
+        const { brand, model, sign, fabrication, form, type, version } = values
+        try {
+            const response = await fetch('http://localhost:3001/create-car', {
+                method: 'post',
+                headers: {
+                    Accept: 'application/json',
                     'Content-type': 'application/json',
-            },
-            body: JSON.stringify({ 
-                brand,
-                model,
-                sign,
-                fabrication,
-                form,
-                type,
-                version
-            }),
-        })
-        const json = await response.json()
-        console.log(json)
-        setSuccess('true')
-    } catch (err) {
-        console.log(err)
-        setSuccess('false')
+                },
+                body: JSON.stringify({
+                    brand,
+                    model,
+                    sign,
+                    fabrication,
+                    form,
+                    type,
+                    version
+                }),
+            })
+            const json = await response.json()
+            console.log(json)
+            setSuccess('true')
+        } catch (err) {
+            console.log(err)
+            setSuccess('false')
+        }
+        // values.event?.preventDefault()
     }
-    // values.event?.preventDefault()
-  }
 
-  const [colorSelect, setColorSelect] = useState('')
+    const [colorSelect, setColorSelect] = useState('')
 
-  const handleSelecteColor = (color: string) => {
-    setColorSelect(color)
-  }
+    const handleSelecteColor = (color: string) => {
+        setColorSelect(color)
+    }
 
-  return (
-    <Container> 
-        <h1 style={{ textAlign: 'center' }}>
-            CREATE
-        </h1>
-        {success === 'true' ? (
-            <p style={{ color: 'green', textAlign: 'center' }}>
-                Carro cadastro realizado com sucesso ✔
-            </p>
-        ) : success === 'false' ? (
-            <p style={{ color: 'red', textAlign: 'center' }}>
-                Ocorreu um erro no cadastro ❌
-            </p>
-        ) : (<p></p> )}
-        <ContentForm>
+    return (
+        <Container>
+            <h1 style={{ textAlign: 'center' }}>
+                CREATE
+            </h1>
+            {success === 'true' ? (
+                <p style={{ color: 'green', textAlign: 'center' }}>
+                    Carro cadastro realizado com sucesso ✔
+                </p>
+            ) : success === 'false' ? (
+                <p style={{ color: 'red', textAlign: 'center' }}>
+                    Ocorreu um erro no cadastro ❌
+                </p>
+            ) : (<p></p>)}
+            <ContentForm>
 
-            <NextLink href="/" passHref>
-                <AiOutlineClose 
-                    size="24" 
-                    style={{
-                        color: 'red', 
-                        backgroundColor: 'transparent', 
-                        marginBottom: ' 0 10px',
-                        justifyContent: 'flex-end',
-                        cursor: 'pointer'
-                    }} 
-                />
-            </NextLink>
+                <NextLink href="/" passHref>
+                    <AiOutlineClose
+                        size="24"
+                        style={{
+                            color: 'red',
+                            backgroundColor: 'transparent',
+                            marginBottom: ' 0 10px',
+                            justifyContent: 'flex-end',
+                            cursor: 'pointer'
+                        }}
+                    />
+                </NextLink>
 
-            <Form
-                method="post" 
-                onSubmit={handleSubmit(handleSubmitForm)}
-            >
-                <ContentInput>
-                    <SelectContent>
-                        <SelectOption {...register('type')}>
-                            <Option value="carro">
-                                Carro
-                            </Option>
-                            <Option value="moto">
-                                Moto
-                            </Option>
-                        </SelectOption>
-                    </SelectContent>
-                    {errors.type ? (
-                        <p style={{color: "red"}}>{errors.type?.message}</p>
-                    ) : (
-                        <p style={{ color: 'transparent' }}>a</p>
-                    )}
-                </ContentInput>
-                
-                <ContentInput>
-                    <InputContainer>
-                        {/* <Label htmlFor='brand'>Marca</Label> */}
-                        <Input
-                            defaultValue="fabrication"
-                            placeholder="Ano de Fabricação"
-                            {...register('fabrication')}
-                        />
-                    </InputContainer>
-                    {errors.fabrication ? (
-                        <p style={{color: "red"}}>{errors.fabrication?.message}</p>
-                    ) : ( 
-                        <p style={{ color: 'transparent' }}>a</p>
-                    )}
-                </ContentInput>
-
-                <ContentInput>
-                    <SelectContent>
-                        <SelectOption
-                            defaultValue=""
-                            {...register('version')}
-                        >
-                            <Option value="Audi">
-                                Audi
-                            </Option>
-                            <Option value="Mercedez">
-                                Mercedez
-                            </Option>
-                        </SelectOption>
-                    </SelectContent>
-                    {errors.version ? (
-                        <p style={{color: "red"}}>{errors.version?.message}</p>
-                    ) : (
-                        <p style={{ color: 'transparent' }}>a</p>
-                    )}
-                </ContentInput>
-
-                <ContentInput>
-                    <InputContainer>
-                        {/* <Label htmlFor='brand'>Marca</Label> */}
-                        <Input
-                            placeholder="Modelo"
-                            {...register('model')}
-                        />
-                    </InputContainer>
-                    {errors.model ? (
-                        <p style={{color: "red"}}>{errors.model?.message}</p>
-                    ) : (
-                        <p style={{ color: 'transparent' }}>a</p>
-                    )}
-                </ContentInput>
-
-                <ContentInput>
-                    <InputContainer>
-                        {/* <Label htmlFor='brand'>Marca</Label> */}
-                        <Input
-                            placeholder="Marca"
-                            {...register('brand')}
-                        />
-                    </InputContainer>
-                    {errors.brand ? (
-                        <p style={{color: "red"}}>{errors.brand?.message}</p>
-                    ) : (
-                        <p style={{ color: 'transparent' }}>a</p>
-                    )} 
-                </ContentInput>
-
-                <ContentInput>
-                    <SelectContent>
-                        <SelectOption {...register('form')}>
-                            <Option value="conversivel">
-                                Conversivel
-                            </Option>
-                            <Option value="sedan">
-                                Sedan
-                            </Option>
-                            <Option value="ret">
-                                Ret
-                            </Option>
-                        </SelectOption>
-                    </SelectContent>
-                    {errors.form ? (
-                        <p style={{color: "red"}}>{errors.form?.message}</p>
+                <Form
+                    method="post"
+                    onSubmit={handleSubmit(handleSubmitForm)}
+                >
+                    <ContentInput>
+                        <SelectContent>
+                            <SelectOption {...register('type')}>
+                                <Option value="carro">
+                                    Carro
+                                </Option>
+                                <Option value="moto">
+                                    Moto
+                                </Option>
+                            </SelectOption>
+                        </SelectContent>
+                        {errors.type ? (
+                            <p style={{ color: "red" }}>{errors.type?.message}</p>
                         ) : (
-                        <p style={{ color: 'transparent' }}>a</p>
-                    ) }
-                </ContentInput>
+                            <p style={{ color: 'transparent' }}>a</p>
+                        )}
+                    </ContentInput>
 
-                <ContentInput>
-                    <InputContainer>
-                        {/* <Label htmlFor='sign'>Placa:</Label> */}
-                        <Input
-                            placeholder="Placa"
-                            id='sign'
-                            {...register('sign')}
-                        />
-                    </InputContainer>
-                    {errors.sign ? (
-                        <p style={{color: "red"}}>{errors.sign?.message}</p>
-                    ) : (
-                        <p style={{ color: 'transparent' }}>a</p>
-                    )}
-                </ContentInput>
-{/*                 
+                    <ContentInput>
+                        <InputContainer>
+                            {/* <Label htmlFor='brand'>Marca</Label> */}
+                            <Input
+                                defaultValue="fabrication"
+                                placeholder="Ano de Fabricação"
+                                {...register('fabrication')}
+                            />
+                        </InputContainer>
+                        {errors.fabrication ? (
+                            <p style={{ color: "red" }}>{errors.fabrication?.message}</p>
+                        ) : (
+                            <p style={{ color: 'transparent' }}>a</p>
+                        )}
+                    </ContentInput>
+
+                    <ContentInput>
+                        <SelectContent>
+                            <SelectOption
+                                defaultValue=""
+                                {...register('version')}
+                            >
+                                <Option value="Audi">
+                                    Audi
+                                </Option>
+                                <Option value="Mercedez">
+                                    Mercedez
+                                </Option>
+                            </SelectOption>
+                        </SelectContent>
+                        {errors.version ? (
+                            <p style={{ color: "red" }}>{errors.version?.message}</p>
+                        ) : (
+                            <p style={{ color: 'transparent' }}>a</p>
+                        )}
+                    </ContentInput>
+
+                    <ContentInput>
+                        <InputContainer>
+                            {/* <Label htmlFor='brand'>Marca</Label> */}
+                            <Input
+                                placeholder="Modelo"
+                                {...register('model')}
+                            />
+                        </InputContainer>
+                        {errors.model ? (
+                            <p style={{ color: "red" }}>{errors.model?.message}</p>
+                        ) : (
+                            <p style={{ color: 'transparent' }}>a</p>
+                        )}
+                    </ContentInput>
+
+                    <ContentInput>
+                        <InputContainer>
+                            {/* <Label htmlFor='brand'>Marca</Label> */}
+                            <Input
+                                placeholder="Marca"
+                                {...register('brand')}
+                            />
+                        </InputContainer>
+                        {errors.brand ? (
+                            <p style={{ color: "red" }}>{errors.brand?.message}</p>
+                        ) : (
+                            <p style={{ color: 'transparent' }}>a</p>
+                        )}
+                    </ContentInput>
+
+                    <ContentInput>
+                        <SelectContent>
+                            <SelectOption {...register('form')}>
+                                <Option value="conversivel">
+                                    Conversivel
+                                </Option>
+                                <Option value="sedan">
+                                    Sedan
+                                </Option>
+                                <Option value="ret">
+                                    Ret
+                                </Option>
+                            </SelectOption>
+                        </SelectContent>
+                        {errors.form ? (
+                            <p style={{ color: "red" }}>{errors.form?.message}</p>
+                        ) : (
+                            <p style={{ color: 'transparent' }}>a</p>
+                        )}
+                    </ContentInput>
+
+                    <ContentInput>
+                        <InputContainer>
+                            {/* <Label htmlFor='sign'>Placa:</Label> */}
+                            <Input
+                                placeholder="Placa"
+                                id='sign'
+                                {...register('sign')}
+                            />
+                        </InputContainer>
+                        {errors.sign ? (
+                            <p style={{ color: "red" }}>{errors.sign?.message}</p>
+                        ) : (
+                            <p style={{ color: 'transparent' }}>a</p>
+                        )}
+                    </ContentInput>
+                    {/*                 
                 <ContentInput>
                     <ContainerColor color={colorSelect}>
                         {['red', 'blue', 'black', 'white', 'gray'].map((color, index) => 
@@ -320,43 +320,43 @@ const Create: NextPage = () => {
                     )}
                 </ContentInput>
                  */}
-                <ContentImage>
-                    {colorSelect === 'blue' 
-                        ? (
-                            <Image src="/images/car_blue.png" /> 
-                        ) : colorSelect === 'red' ? (
-                            <Image src="/images/car_red.png/" />
-                        ) : colorSelect === 'white' ? (
-                            <Image src="/images/car_white.png/" />
-                        ) : colorSelect === 'black' ? (
-                            <Image src="/images/car_black.png/" />
-                        ) : colorSelect === 'gray' ? (
-                            <Image src="/images/car_gray.png/" />
-                        ): ( <></> )
-                    }
-                </ContentImage>
-                
-                <ContainerAction>
+                    <ContentImage>
+                        {colorSelect === 'blue'
+                            ? (
+                                <Image src="/images/car_blue.png" />
+                            ) : colorSelect === 'red' ? (
+                                <Image src="/images/car_red.png/" />
+                            ) : colorSelect === 'white' ? (
+                                <Image src="/images/car_white.png/" />
+                            ) : colorSelect === 'black' ? (
+                                <Image src="/images/car_black.png/" />
+                            ) : colorSelect === 'gray' ? (
+                                <Image src="/images/car_gray.png/" />
+                            ) : (<></>)
+                        }
+                    </ContentImage>
 
-                    <Button type="submit">
-                        <Text>Create</Text>
-                        <AiOutlineCar size={24} />
-                    </Button>
+                    <ContainerAction>
 
-                    <NextLink href="/" passHref>
                         <Button type="submit">
-                            <Text>Voltar</Text>
-                            <AiOutlineBackward size={24} />
+                            <Text>Create</Text>
+                            <AiOutlineCar size={24} />
                         </Button>
-                    </NextLink>
 
-                </ContainerAction>
+                        <NextLink href="/" passHref>
+                            <Button type="submit">
+                                <Text>Voltar</Text>
+                                <AiOutlineBackward size={24} />
+                            </Button>
+                        </NextLink>
 
-            </Form>
+                    </ContainerAction>
 
-        </ContentForm>
-    </Container>
-  )
+                </Form>
+
+            </ContentForm>
+        </Container>
+    )
 }
 
 export default Create
--- front-end/src/pages/cars/index.tsx
+++ front-end/src/pages/cars/index.tsx
@@ -31,12 +31,12 @@ interface Props {
   }[]
 }
 
-const Cars = ( props : Props) => {
+const Cars = (props: Props) => {
 
   const [cars, setCars] = useState({});
   useEffect(() => {
     setCars(props)
-  },[props])
+  }, [props])
 
   console.log("cars:")
   console.log(cars)
@@ -53,46 +53,46 @@ const Cars = ( props : Props) => {
       <Container>
 
         <ContainerSearchInput>
-          <SearchInput 
+          <SearchInput
             // defaultValue="" 
-            value={search} 
+            value={search}
             onChange={(e) => setSearch(e.target.value)}
           />
           <SearchIcon />
         </ContainerSearchInput>
 
-        { search !== "" ? props.data.filter(( item ) => item.brand.includes(search) ).map(filterYear => (
-            <Card key={filterYear._id}>
-              <Text>CARRO</Text>
-              <ContentInfo>
-                  <TextInfo>Marca: {filterYear.brand}</TextInfo>
-                  {/* <TextInfo>Id: {item._id}</TextInfo> */}
-                  <TextInfo>Modelo: {filterYear.model}</TextInfo>
-                  <TextInfo>Ano de fabricação: {filterYear.fabrication}</TextInfo>
-                  <TextInfo>Cor</TextInfo>
-                  <TextInfo>forma: {filterYear.form}</TextInfo>
-                  <TextInfo>Placa: {filterYear.sign}</TextInfo>
-              </ContentInfo>
-              <NextLink href={`/cars/${filterYear._id}`} passHref>
-                <Button>
-                  <Text>Edit</Text>
-                  <AiOutlineEdit size={24} />
-                </Button>
-              </NextLink>
+        {search !== "" ? props.data.filter((item) => item.brand.includes(search)).map(filterYear => (
+          <Card key={filterYear._id}>
+            <Text>CARRO</Text>
+            <ContentInfo>
+              <TextInfo>Marca: {filterYear.brand}</TextInfo>
+              {/* <TextInfo>Id: {item._id}</TextInfo> */}
+              <TextInfo>Modelo: {filterYear.model}</TextInfo>
+              <TextInfo>Ano de fabricação: {filterYear.fabrication}</TextInfo>
+              <TextInfo>Cor</TextInfo>
+              <TextInfo>forma: {filterYear.form}</TextInfo>
+              <TextInfo>Placa: {filterYear.sign}</TextInfo>
+            </ContentInfo>
+            <NextLink href={`/cars/${filterYear._id}`} passHref>
+              <Button>
+                <Text>Edit</Text>
+                <AiOutlineEdit size={24} />
+              </Button>
+            </NextLink>
           </Card>
         )) : (
-        <Content>
-          {props.data.map((item ,index) => item && (
+          <Content>
+            {props.data.map((item, index) => item && (
               <Card key={index}>
                 <Text>CARRO</Text>
                 <ContentInfo>
-                    <TextInfo>Marca: {item.brand}</TextInfo>
-                    {/* <TextInfo>Id: {item._id}</TextInfo> */}
-                    <TextInfo>Modelo: {item.model}</TextInfo>
-                    <TextInfo>Ano de fabricação: {item.fabrication}</TextInfo>
-                    <TextInfo>Cor</TextInfo>
-                    <TextInfo>forma: {item.form}</TextInfo>
-                    <TextInfo>Placa: {item.sign}</TextInfo>
+                  <TextInfo>Marca: {item.brand}</TextInfo>
+                  {/* <TextInfo>Id: {item._id}</TextInfo> */}
+                  <TextInfo>Modelo: {item.model}</TextInfo>
+                  <TextInfo>Ano de fabricação: {item.fabrication}</TextInfo>
+                  <TextInfo>Cor</TextInfo>
+                  <TextInfo>forma: {item.form}</TextInfo>
+                  <TextInfo>Placa: {item.sign}</TextInfo>
                 </ContentInfo>
                 <NextLink href={`/cars/${item._id}`} passHref>
                   <Button>
@@ -100,10 +100,10 @@ const Cars = ( props : Props) => {
                     <AiOutlineEdit size={24} />
                   </Button>
                 </NextLink>
-            </Card>
+              </Card>
             ))}
           </Content>
-      )}
+        )}
       </Container>
     </>
   )
@@ -111,8 +111,8 @@ const Cars = ( props : Props) => {
 
 export default Cars
 
-export const getServerSideProps = async ( ) => {
-  const response = await fetch(`http://localhost:3001/cars`);
+export const getServerSideProps = async () => {
+  const response = await fetch(`https://car-management-production-c408.up.railway.app/`);
   const data: Props = await response.json();
 
   console.log("data preview:");
